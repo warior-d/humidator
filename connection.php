818474a0717087de08ef81744782d953a0289c25
warior-d <haloy@yandex.ru>
2021-10-11 17:26:07 +0300

    Поправил коннекшн

--- connection.php
+++ connection.php
@@ -1,6 +1,11 @@
 <?php
-$host = 'localhost'; // адрес сервера 
+/* $host = 'localhost'; // адрес сервера 
 $database = 'f0520793_INC'; // имя базы данных
 $user = 'f0520793_INC'; // имя пользователя
-$password = 'Didi1683759'; // пароль
+$password = 'Didi1683759'; // пароль */
+
+$host = 'localhost'; // адрес сервера 
+$database = 'postgres_main'; // имя базы данных
+$user = 'humi_dk'; // имя пользователя
+$password = 'flashback'; // пароль
 ?>
