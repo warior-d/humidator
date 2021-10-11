32b151861e50c07782071ba806eead5fa9b62748
warior-d <haloy@yandex.ru>
2021-10-11 17:32:48 +0300

    last commit

--- core_graph/core_gr.php
+++ core_graph/core_gr.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once './connection.php';
+/* require_once './connection.php';
 
     $mysqli = new mysqli('localhost','f0520793_INC','Didi1683759','f0520793_INC');
     if ($mysqli->connect_error) {
@@ -20,5 +20,26 @@ require_once './connection.php';
 function getData(){
     echo json_encode("111");
 }
+ */
+ 
+require_once './connection.php';
+
+$dbconn = pg_connect("host=".$host." dbname=".$database." user=".$user." password=".$password);
+
+$query_db = 'SELECT SENSOR, TEMPERATURE, HUMIDITY, PRESSURE, DATE_LINUX 
+    FROM wifi_humidyty.humidata 
+    WHERE DATE_LINUX = (SELECT MAX(DATE_LINUX) FROM wifi_humidyty.humidata)';
+$query_result = pg_query($query_db);
+ 
+$result = pg_fetch_all($query_result);
+
+$temp = $result['0']['temperature'];
+$pres = $result['0']['pressure'];
+$humi = $result['0']['humidity'];
+
+
+$date =  date('d.m.Y H:i:s', $result['0']['date_linux']);
+$date_diff = time() - $result['0']['date_linux']; 
+
 
 ?>
